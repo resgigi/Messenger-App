ef8752fca747dc4115787105c8b192bf3ff59ae3
2v <matran00454@gmail.com>
2020-10-16 13:27:46 +0700

    login

--- Messenger-App/ViewController.swift
+++ Messenger-App/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 class ViewController: UIViewController  {
 
     @IBOutlet weak var _txtUsername: UITextField!
-    
     @IBOutlet weak var _txtPassword: UITextField!
     @IBOutlet weak var _BtnLogin: UIButton!
+    @IBOutlet var View_login: UIView!
+    @IBOutlet var View_Register: UIView!
+    @IBOutlet weak var _btnRegister: UIButton!
+    
+    @IBOutlet weak var _linkRegister: UITextView!
+    
+    @IBOutlet weak var _btnBackLogin: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,6 +27,10 @@ class ViewController: UIViewController  {
     
     @IBAction func _ACLogin(_ sender: Any) {
         _txtUsername.text = "lalala"
+        if _linkRegister.isTracking == true {
+            View_Register.isHidden = true
+        }
+        
     }
     
 }
